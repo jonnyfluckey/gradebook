9d0b96c3b3fdfeddad29f82c354166c7083ca619
Jonny Fluckey <46408080+jonnyfluckey@users.noreply.github.com>
2019-09-25 15:35:13 -0600

    added letter grade function

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -11,9 +11,50 @@ namespace GradeBook
        grades = new List<double>();
        Name = name;
     }
+    
+    public void AddLetterGrade(char letter)
+    {
+        switch(letter)
+        {
+          case 'A':
+            AddGrade(90);
+            break;
+            
+          case 'B':
+            AddGrade(80);
+            break;
+          
+          case 'C':
+            AddGrade(70);
+            break;
+            
+          case 'D':
+            AddGrade(60);
+            break;
+            
+          case 'F':
+            AddGrade(50);
+            break;
+            
+          default:
+            AddGrade(0);
+            break;
+        }
+        
+    }
+    
     public void AddGrade(double grade)
     {
-        grades.Add(grade);
+        if(grade <= 100 && grade >= 0)
+        {
+            grades.Add(grade);
+        }
+        else
+        {
+            Console.WriteLine("Invalid Value");
+        }
+      
+      
     }
     public Statistics GetStatistics()
     {
@@ -21,11 +62,21 @@ namespace GradeBook
             result.Average = 0.0;
             result.High = double.MinValue;
             result.Low = double.MaxValue;
-            foreach(var grade in grades) {
-                result.Low = Math.Min(grade, result.Low);
-                result.High = Math.Max(grade, result.High);
-                result.Average += grade;
+            
+            for(var index = 0; index < grades.Count; index++)
+            {
+                if(grades[index] == 42.1)
+                {
+                  // break
+                  continue;
+                  // goto "label"; "not used very often"
+                }
+              
+                result.Low = Math.Min(grades[index], result.Low);
+                result.High = Math.Max(grades[index], result.High);
+                result.Average += grades[index];
             }
+            
             result.Average /= grades.Count;
             
             return result;
